b715ebc546987a34f597d192a3ae7e0710725725
luis beqja <luisbeqjamw@gmail.com>
2024-09-25 12:56:52 +0200

    Ex 02.02 numbers v1

---
+++ Programming_1/Numbers/Numbers_v1.java
@@ -0,0 +1,19 @@
+package Programming_1.Numbers;
+
+public class Numbers_v1 {
+    public static void main(String[] args) {
+        long firstNumber = 2000000000;
+        long secondNumber = 2000000000;
+
+        //System.out.println(firstNumber + secondNumber);
+
+        long thirdNumber = 10000;
+        long fourthNumber = 10000;
+        int result;
+        result = (int)(thirdNumber + fourthNumber);
+
+        //System.out.println(result);
+
+
+    }
+}
---
+++ Programming_1/Numbers/Numbers_v1_2.java
@@ -0,0 +1,19 @@
+package Programming_1.Numbers;
+
+public class Numbers_v1_2 {
+    public static void main(String[] args) {
+        int first = 8, second = 5;
+
+/*        System.out.println(first + second);
+        System.out.println(first - second);
+        System.out.println(first * second);
+        System.out.println(first / second);*/
+
+        int result;
+
+        System.out.println(result = ++first);
+        System.out.println(result = first++);
+        System.out.println(result = --second);
+        System.out.println(result = second--);
+    }
+}
